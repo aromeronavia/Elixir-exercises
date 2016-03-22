df867fb43fbd62021f67c6359649fdb4a689bc4a
Setzer777 <lguitarras0594@gmail.com>
2016-03-21 21:33:12 -0600

    First Commit

---
+++ palindromes/README.md
@@ -0,0 +1,20 @@
+# Palindromes
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add palindromes to your list of dependencies in `mix.exs`:
+
+        def deps do
+          [{:palindromes, "~> 0.0.1"}]
+        end
+
+  2. Ensure palindromes is started before your application:
+
+        def application do
+          [applications: [:palindromes]]
+        end
+
---
+++ palindromes/lib/palindromes.ex
@@ -0,0 +1,25 @@
+defmodule Palindromes do
+  @doc "Returns true if string is a palindrome"
+  def palindrome?(a) when is_binary(a) do
+    IO.puts palindrome([{:string, a},
+                        {:start, 0},
+                        {:end, String.length(a) - 1}])
+  end
+
+  defp palindrome(args) do
+    if args[:start] < args[:end] do
+      chars_equals?(args[:string], args[:start], args[:end])
+      and palindrome([{:string, args[:string]},
+                      {:start, args[:start] + 1},
+                      {:end, args[:end] - 1}])
+    else
+      chars_equals?(args[:string], args[:start], args[:end])
+    end
+  end
+
+  defp chars_equals?(string, s, e) do
+    String.at(string, s) == String.at(string, e)
+  end
+end
+
+Palindromes.palindrome?("aabaa")
